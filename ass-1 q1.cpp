d8397720d6fb61633bc9a570c71e94ef60a76fa3
lucky22103187 <22103187@MAIL.JIIT.AC.IN>
2024-07-24 16:04:53 +0530

    Add files via upload

---
+++ ass-1 q1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namepsce std;
+
+void calculate_avg(int arr, int n)
+{
+    for(int i ;i<=n;i++)
+    {
+     int sum+=arr[i];
+
+    }
+}
+
+
+
+
+int main(){
+
+int n;
+cout<<"hello everyone ";
+cin>>n;
+
+for(int i=0; i<=n;i++)
+    {
+    int num+=arr[i]
+
+}
+
+
+}
