7fa5c8a884313d3d3efaef21d26dea9c5052f85b
lucky22103187 <22103187@MAIL.JIIT.AC.IN>
2024-07-25 00:19:02 +0530

    ass-1q2.cpp

--- ass-1q2.cpp
+++ ass-1q2.cpp
@@ -1,5 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
+
+
+int main() {
+    string input;
+
+    cout << "Enter a string: ";
+    getline(std::cin, input);
+
+    reverse(input.begin(), input.end());
+
+    cout << "Reversed string: " << input << std::endl;
+
+    return 0;
+}
 
